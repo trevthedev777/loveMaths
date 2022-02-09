6d8c0e9a1d239e87807fcdf0566847d2599a29af
trevthedev777 <bloodsportrecords@gmail.com>
2022-02-09 07:37:41 +0000

    display additrtion question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,28 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert('You clicked Submit!');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame('addition');
 });
 
-function runGame(){
+function runGame(gameType) {
 
     // create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
-}
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+        } else {
+            alert(`Unknown game: ${gameType}`) ;
+            throw `Unknown game type: ${gameType}. Aborting!`;
+        }
+    }
+
 
 function checkAnswer() {
 
@@ -40,8 +50,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
